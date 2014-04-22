a10dc19cd4743f68569bca91b84a84b49568f227
John Horan <j.horan@4c.ucc.ie>
2014-04-22 12:30:27 +0100

    Pushback

--- pulseinterface.js
+++ pulseinterface.js
@@ -8,14 +8,39 @@ const PAServerLookupIface = '<node>\
 	</node>';
 const ServerLookupProxy = Gio.DBusProxy.makeProxyWrapper(PAServerLookupIface);
 
+const PAIface = '<node>\
+		<interface name="org.PulseAudio.Core1">\
+			<property name="Name" type="s" access="read" />\
+		</interface>\
+	</node>';
+const PAProxy = Gio.DBusProxy.makeProxyWrapper(PAIface);
 
+
+const IntrospectProxy = Gio.DBusProxy.makeProxyWrapper('<node>\
+		<interface name="org.freedesktop.DBus.Introspectable">\
+			<method name="Introspect">\
+				<arg name="data" type="s" direction="out"/>\
+			</method>\
+		</interface>\
+	</node>');
 
 function getServerAddress(){
 	let conn = new ServerLookupProxy(Gio.DBus.session, 'org.PulseAudio1', '/org/pulseaudio/server_lookup1');
 	return conn.Address;
+}
+
+function getPulseAudioConnection(){
+	
+	let paProxy = Gio.DBusConnection.new_for_address_sync(this.getServerAddress(), Gio.DBusConnectionFlags.NONE, null, null);
+//print(paProxy);
+//let conn = new PAProxy(paProxy, 'org.PulseAudio', '/org/pulseaudio/core1');
+//	print(conn.Name);
+
+	let introspect = new IntrospectProxy(paProxy, 'org.freedesktop.DBus.Introspectable', '/org/pulseaudio/core1');
+	print(introspect.IntrospectSync());
+
 
 }
 
-print(Gio.DBus.session);
 
-this.getServerAddress();
+this.getPulseAudioConnection();
